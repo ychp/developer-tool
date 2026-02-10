c768383884b031ab11df44a5dd6165f6dbf30aec
yingchengpeng <yingchengpeng@youzan.com>
2026-02-10 14:52:09 +0800

    fix: 修复 Button 组件 asChild 属性警告
    - 从 props 中解构 asChild 属性
    - 防止 asChild 传递到 DOM 元素
    - 消除 React 警告信息

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -39,7 +39,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, asChild, ...props }, ref) => {
     return (
       <button
         className={cn(buttonVariants({ variant, size, className }))}
