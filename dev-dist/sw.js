fa33e03f1f74f05ea0acb52e72be4143347b565b
yingchengpeng <yingchengpeng@youzan.com>
2026-03-10 15:46:49 +0800

    更新

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g98rp57d0d8"
+    "revision": "0.b70d2opneho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
